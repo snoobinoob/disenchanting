73742c23c55af5ee4582d21b2aa9589fbebdb5c3
Snoobinoob <cheeseycheese99@gmail.com>
2023-02-22 20:34:50 -0500

    Initial commit

---
+++ src/main/java/disenchanting/Disenchanting.java
@@ -0,0 +1,11 @@
+package disenchanting;
+
+import necesse.engine.modLoader.annotations.ModEntry;
+
+@ModEntry
+public class Disenchanting {
+
+    public void init() {
+        System.out.println("Hello world from Disenchanting!");
+    }
+}
